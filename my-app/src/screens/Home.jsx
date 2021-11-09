7b4fab9db7dcdc73e142b516b2eeb63bca390741
Jared Beneroff <85003025+jbeneroff@users.noreply.github.com>
2021-11-09 09:41:38 -0800

    home edits

--- my-app/src/screens/Home.jsx
+++ my-app/src/screens/Home.jsx
@@ -13,7 +13,8 @@ export default function Home() {
       <div>
         <h1 id='name'>Cobalt (CBLT)</h1>
         <h3 id='token'>A Binance Smart Chain Token</h3>
-        <h5>Contract Address: 0x599a2070482CA75e5F76B5F3c94A6dc08bdEC6F3</h5>
+        <h5 id='address'>Contract Address: 0x599a2070482CA75e5F76B5F3c94A6dc08bdEC6F3</h5>
+        <h6 id='wallet'>We recommend setting up a Trust or Metamask wallet and adding the token by the contract address.</h6>
         <p id='info'>Cobalt is an cat-themed token built on the Binance Smart Chain for the benefits of near instantaneous transaction speed, and bridging capabilities across various dApps and networks. Its purpose is to be a rigid Store of Value (SoV) digital asset.</p>
       </div>
       <div className={styles.btns}>
