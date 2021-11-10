66ecf630ca8d01c613d3ad49389036821cde0295
Jared Beneroff <85003025+jbeneroff@users.noreply.github.com>
2021-11-10 18:20:15 -0500

    text updated

--- my-app/src/screens/Home.jsx
+++ my-app/src/screens/Home.jsx
@@ -15,7 +15,9 @@ export default function Home() {
         <h3 id='token'>A Binance Smart Chain Token</h3>
         <h5 id='address'>Contract Address: 0x599a2070482CA75e5F76B5F3c94A6dc08bdEC6F3</h5>
         <h6 id='wallet'>We recommend setting up a Trust or Metamask wallet and adding the token by the contract address.</h6>
-        <p id='info'>Cobalt is an cat-themed token built on the Binance Smart Chain for the benefits of near instantaneous transaction speed, and bridging capabilities across various dApps and networks. Its purpose is to be a rigid Store of Value (SoV) digital asset.</p>
+        <p id='info'>Cobalt (CBLT) is a cat-themed BEP-20 token built on the Binance Smart Chain for the benefit of near instantaneous transaction speed.
+          Its purpose is to be a rigid Store of Value (SoV) digital asset. The total supply of CBLT is 1 Billion tokens.
+          These will be gradually introduced to the market as the development team uses them to expand the Cobalt ecosystem.</p>
       </div>
       <div className={styles.btns}>
         {/* <Link to ='/NFT' className='nav-item'>
