e6d9d3fcc8296fac1ac805c297150989a82617c9
Jared Beneroff <85003025+jbeneroff@users.noreply.github.com>
2021-11-10 12:08:16 -0500

    button edits

--- my-app/src/screens/Contact.jsx
+++ my-app/src/screens/Contact.jsx
@@ -13,28 +13,28 @@ export default function Contact() {
       <h2>Join the Discussion</h2>
       <p id='contact-info'>We believe staying anonymous will help the progression of the various products in the pipeline and the overall distribution of our awareness. However we don’t want to totally cut ourselves off from our community and curious cats around the world. We have many ways to be reached!</p>
       <div className='socials'>
-        <a id='twitter' href="https://twitter.com/CBLTcoin">
-          <button className='social-btn'>Twitter</button>
+        <a className='social-btn' id='twitter' href="https://twitter.com/CBLTcoin">
+          Twitter
         </a>
-        <a id='insta' href="https://www.instagram.com/cbltcoin/">
-          <button className='social-btn'>Instagram</button>
+        <a className='social-btn' id='insta' href="https://www.instagram.com/cbltcoin/">
+          Instagram
         </a>
-        <a id='discord' href="https://discord.gg/FwJ5Xkfj">
-          <button className='social-btn'>Discord</button>
+        <a className='social-btn' id='discord' href="https://discord.gg/FwJ5Xkfj">
+          Discord
         </a>
-        <a id='reddit' href="https://www.reddit.com/r/cblt">
-          <button className='social-btn'>Reddit</button>
+        <a className='social-btn' id='reddit' href="https://www.reddit.com/r/cblt">
+          Reddit
         </a>
       </div>
       <h3 id='touch'>Get in Touch with the Developer</h3>
       <div className='contact'>
         <h4 id='name'>Founder and Lead Developer:  Jared Beneroff</h4>
         <p id='email'>Email:  admin@cbltcoin.com</p>
-        <a id='linkedin' href="https://www.linkedin.com/in/jared-beneroff/">
-          <button className='social-btn'>LinkedIn</button>
+        <a className='social-btn' id='linkedin' href="https://www.linkedin.com/in/jared-beneroff/">
+          LinkedIn
         </a>
-        <a id='github' href="https://github.com/jbeneroff">
-          <button className='social-btn'>Github</button>
+        <a className='social-btn' id='github' href="https://github.com/jbeneroff">
+          Github
         </a>
       </div>
     </div>
